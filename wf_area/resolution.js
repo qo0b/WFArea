3d887dc207ec3cacdb481f8d1b95cae157403c88
snackwf <100385915+snackwf@users.noreply.github.com>
2023-07-28 11:13:43 +0200

    Add files via upload

---
+++ wf_area/resolution.js
@@ -0,0 +1,20 @@
+var resolutions = {
+	"Standard": {
+		"width": "720",
+		"height": "1280",
+		"lwidth": "360",
+		"lheight": "640"
+	},
+	"iPhone": {
+		"width": "750",
+		"height": "1334",
+		"lwidth": "375",
+		"lheight": "667"
+	},
+	"Samsung Galaxy": {
+		"width": "1440",
+		"height": "2960",
+		"lwidth": "360",
+		"lheight": "740"
+	}
+}
